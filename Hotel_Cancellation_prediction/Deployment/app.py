4f53089ea672d7e255dbb182c043e17686960c09
Shubham <shubhamsharma15104@gmail.com>
2024-12-17 12:35:42 +0530

    Update app.py

--- Hotel_Cancellation_prediction/Deployment/app.py
+++ Hotel_Cancellation_prediction/Deployment/app.py
@@ -11,7 +11,6 @@ with open('transformer.pkl','wb') as file:
 
 # [lt_t,mst,spcl,price_t,adults,wkd,park,wk,month,day,weekd]
 def prediction(input_list):
-    input_list=np.array(input_list,dtype=object)
     pred=model.predict(input_list)
     pred_prob=model.predict_proba([input_list])[:,1][0] # probability of 1
     if pred_prob>0.5:
@@ -33,7 +32,7 @@ def main():
     day=st.slider('What will be the Day of Arival?',min_value=1,max_value=31,step=1)
     wkday_lambda=(lambda x:0 if x=='Mon' else 1 if x=='Tue' else 2 if x=='Wed' else 3 if x=='Thrus' else 4 if x=='Fri' else 5 if x=='Sat' else 6)
     wkday=wkday_lambda(st.selectbox('What is the Weekday of arival?'['Mon','Tue','Wed','Thrus','Fri','Sat','Sun']))
-    tran_data=pt.transform([[lt,price]])[0]
+    tran_data=pt.transform([[float(lt),float(price)]])[0]
     lt_t=tran_data[0]
     price_t=tran_data[1]
 
